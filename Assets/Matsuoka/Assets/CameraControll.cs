3c4d0fa1f0a01d8fb33be53907d825f0aed1844c
cshm225 <cshm225@gmail.com>
2022-08-12 13:19:46 +0900

    1360

---
+++ Assets/Matsuoka/Assets/CameraControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float speed = 1;
+    public float rotX = 0f;
+    public float rotY = 0f;
+    public float distance = 5f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotX -= Input.GetAxis("Mouse Y") * speed;
+        rotY += Input.GetAxis("Mouse X") * speed;
+        rotX = Mathf.Clamp(rotX, 8f, 60f);
+        rotY = Mathf.Repeat(rotY, 360f);
+
+        Vector3 lookPosition = player.transform.position;
+        Vector3 relativePos = Quaternion.Euler(rotX, rotY, 0) * new Vector3(0, 0, -distance);
+        transform.position=lookPosition+relativePos;
+        transform.LookAt(lookPosition);
+    }
+}
---
+++ Assets/Matsuoka/Assets/Play.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public Maze map;
+    public float animSpeed = 1.5f;              // アニメーション再生速度設定
+    public float lookSmoother = 3.0f;           // a smoothing setting for camera motion
+    public bool useCurves = true;               // Mecanimでカーブ調整を使うか設定する
+                                                // このスイッチが入っていないとカーブは使われない
+    public float useCurvesHeight = 0.5f;        // カーブ補正の有効高さ（地面をすり抜けやすい時には大きくする）
+
+    // 以下キャラクターコントローラ用パラメタ
+    // 前進速度
+    public float forwardSpeed = 7.0f;
+    // 後退速度
+    public float backwardSpeed = 2.0f;
+    // 旋回速度
+    public float rotateSpeed = 2.0f;
+    // ジャンプ威力
+    public float jumpPower = 3.0f;
+    // キャラクターコントローラ（カプセルコライダ）の参照
+    private CapsuleCollider col;
+    private Rigidbody rb;
+    // キャラクターコントローラ（カプセルコライダ）の移動量
+    private Vector3 velocity;
+    // CapsuleColliderで設定されているコライダのHeiht、Centerの初期値を収める変数
+    private float orgColHight;
+    private Vector3 orgVectColCenter;
+    private Animator anim;                          // キャラにアタッチされるアニメーターへの参照
+    private AnimatorStateInfo currentBaseState;         // base layerで使われる、アニメーターの現在の状態の参照
+
+    private GameObject cameraObject;    // メインカメラへの参照
+
+    // アニメーター各ステートへの参照
+    static int idleState = Animator.StringToHash("Base Layer.Idle");
+    static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+    static int jumpState = Animator.StringToHash("Base Layer.Jump");
+    static int restState = Animator.StringToHash("Base Layer.Rest");
+
+    // 初期化
+    void Start()
+    {
+        // Animatorコンポーネントを取得する
+        anim = GetComponent<Animator>();
+        // CapsuleColliderコンポーネントを取得する（カプセル型コリジョン）
+        col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        //メインカメラを取得する
+        cameraObject = GameObject.FindWithTag("MainCamera");
+        // CapsuleColliderコンポーネントのHeight、Centerの初期値を保存する
+        orgColHight = col.height;
+        orgVectColCenter = col.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        velocity = new Vector3(h,0,v);
+        velocity = transform.TransformDirection(velocity);
+        if (v > 0.1 || h != 0)
+        {
+            velocity *= forwardSpeed;       // 移動速度を掛ける
+        }
+        else if (v < -0.1)
+        {
+            velocity *= backwardSpeed;  // 移動速度を掛ける
+        }
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+    }
+}
