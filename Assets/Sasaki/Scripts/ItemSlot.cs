08d4e43dbd09663293abe251472bff932f045f77
sasakiy916 <sasaki3y98521j@gmail.com>
2022-08-22 22:28:32 +0900

    アイテム選択、選択アイテムの使用　追加

--- Assets/Sasaki/Scripts/ItemSlot.cs
+++ Assets/Sasaki/Scripts/ItemSlot.cs
@@ -6,12 +6,31 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
+    public enum TYPE
+    {
+        KEY,
+        HINT
+    }
+    public TYPE type;
     public new string name;//名前
     public Sprite image;//画像
     public ItemSlots itemList;//取得後に入るアイテムリスト
     public void OnPointerClick(PointerEventData eventData)
     {
         gameObject.SetActive(false);
-        itemList.PickUpItem(this);
+        ItemSlots.instance.PickUpItem(this);
     }
+    //作成中
+    // public bool UseItem()
+    // {
+    //     switch (type)
+    //     {
+    //         case TYPE.KEY:
+    //             return true;
+    //         case TYPE.HINT:
+    //             Debug.Log("ヒントだよ");
+    //             break;
+    //     }
+    //     return false;
+    // }
 }
--- Assets/Sasaki/Scripts/ItemSlots.cs
+++ Assets/Sasaki/Scripts/ItemSlots.cs
@@ -6,14 +6,33 @@ using UnityEngine.UI;
 public class ItemSlots : MonoBehaviour
 {
     public GameObject imagePrefabs;
-    public List<ItemSlot> list = new List<ItemSlot>();
+    // public List<ItemSlot> list = new List<ItemSlot>();
+    public List<GameObject> list = new List<GameObject>();
+    public SlotItem selectItem;
+
+    public static ItemSlots instance;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
     public void PickUpItem(ItemSlot item)
     {
         Debug.Log(item.name);
-        list.Add(item);
+        //Imageオブジェクトを生成し、SlotItemコンポーネントと画像を設定
         GameObject image = Instantiate(imagePrefabs);
-        // image.transform.parent = this.transform;
+        // list.Add(image.GetComponent<ItemSlot>());
+        list.Add(image);
         image.transform.SetParent(this.transform, false);
+        image.AddComponent<SlotItem>();
+        image.GetComponent<SlotItem>().name = item.name;
         image.GetComponent<Image>().sprite = item.image;
+
+        //画像のアルファ値調整
+        Image nantoka = image.GetComponent<Image>();
+        // nantoka.color = new Color(1, 1, 1, 0.4f);
+        nantoka.color = new Color32(255, 255, 255, 100);
     }
 }
