d48042325a8aa0a06e8d33c346d20100454b4ffe
yakan777 <yume_drop@hotmail.com>
2022-08-25 13:36:57 +0900

    commit

--- Assets/Nagano/Scripts/Player.cs
+++ Assets/Nagano/Scripts/Player.cs
@@ -1,105 +1,215 @@
-using System.Collections;
+ using System.Collections;
  using System.Collections.Generic;
  using UnityEngine;
+
  public class Player : MonoBehaviour
  {
-     //インスペクターで設定する
-     public float speed; //速度
-     public float gravity; //重力
-     public float jumpSpeed;//ジャンプする速度
-     public float jumpHeight;//高さ制限
-     public float jumpLimitTime;//ジャンプ制限時間
-     public GroundCheck ground; //接地判定
-     public GroundCheck head;//頭ぶつけた判定
-     public AnimationCurve dashCurve;  //New
-     public AnimationCurve jumpCurve;  //New
+     #region//インスペクターで設定する
+     [Header("移動速度")] public float speed;
+     [Header("重力")] public float gravity;
+     [Header("ジャンプ速度")] public float jumpSpeed;
+     [Header("ジャンプする高さ")] public float jumpHeight;
+     [Header("ジャンプする長さ")] public float jumpLimitTime;
+     [Header("接地判定")] public GroundCheck ground;
+     [Header("天井判定")] public GroundCheck head;
+     [Header("ダッシュの速さ表現")] public AnimationCurve dashCurve;
+     [Header("ジャンプの速さ表現")] public AnimationCurve jumpCurve;
+     [Header("踏みつけ判定の高さの割合(%)")] public float stepOnRate;
+     #endregion
 
-     //プライベート変数
+     #region//プライベート変数
      private Animator anim = null;
      private Rigidbody2D rb = null;
+     private CapsuleCollider2D capcol = null;
+     private SpriteRenderer sr = null;
      private bool isGround = false;
      private bool isJump = false;
      private bool isHead = false;
+     private bool isRun = false;
+     private bool isDown = false;
+     private bool isOtherJump = false;
+     private bool isContinue = false;
      private float jumpPos = 0.0f;
-     private float dashTime, jumpTime;  //New
-     private float beforeKey;  //New
+     private float otherJumpHeight = 0.0f;
+     private float dashTime = 0.0f;
+     private float jumpTime = 0.0f;
+     private float beforeKey = 0.0f;
+     private float continueTime = 0.0f;
+     private float blinkTime = 0.0f;
+     private string enemyTag = "Enemy";
+     #endregion
 
      void Start()
      {
           //コンポーネントのインスタンスを捕まえる
           anim = GetComponent<Animator>();
           rb = GetComponent<Rigidbody2D>();
+          capcol = GetComponent<CapsuleCollider2D>();
+          sr = GetComponent<SpriteRenderer>();
+     }
+
+     private void Update()
+     {
+          if (isContinue)
+          {
+              //明滅　ついている時に戻る
+              if (blinkTime > 0.2f)
+              {
+                  sr.enabled = true;
+                  blinkTime = 0.0f;
+              }
+              //明滅　消えているとき
+              else if (blinkTime > 0.1f)
+              {
+                  sr.enabled = false;
+              }
+              //明滅　ついているとき
+              else
+              {
+                  sr.enabled = true;
+              }
+              //1秒たったら明滅終わり
+              if (continueTime > 1.0f)
+              {
+                  isContinue = false;
+                  blinkTime = 0.0f;
+                  continueTime = 0.0f;
+                  sr.enabled = true;
+              }
+              else
+              {
+                  blinkTime += Time.deltaTime;
+                  continueTime += Time.deltaTime;
+              }
+          }
      }
 
      void FixedUpdate()
      {
-          //接地判定を得る
-          isGround = ground.IsGround();
-          isHead = head.IsGround();
+          if (!isDown)
+          {
+              //接地判定を得る
+              isGround = ground.IsGround();
+              isHead = head.IsGround();
 
-          //キー入力されたら行動する
+              //各種座標軸の速度を求める
+              float xSpeed = GetXSpeed();
+              float ySpeed = GetYSpeed();
+
+              //アニメーションを適用
+              SetAnimation();
+
+              //移動速度を設定
+              rb.velocity = new Vector2(xSpeed, ySpeed);
+          }
+          else
+          {
+              rb.velocity = new Vector2(0, -gravity);
+          }
+     }
+
+     /// <summary>
+     /// Y成分で必要な計算をし、速度を返す。
+     /// </summary>
+     /// <returns>Y軸の速さ</returns>
+     private float GetYSpeed()
+     {
+          float verticalKey = Input.GetAxis("Jump");
+          float ySpeed = -gravity;
+
+          //何かを踏んだ際のジャンプ
+          if (isOtherJump)
+          {
+                //現在の高さが飛べる高さより下か
+                bool canHeight = jumpPos + otherJumpHeight > transform.position.y;
+                //ジャンプ時間が長くなりすぎてないか
+                bool canTime = jumpLimitTime > jumpTime;
+
+                if (canHeight && canTime && !isHead)
+                {
+                    ySpeed = jumpSpeed;
+                    jumpTime += Time.deltaTime;
+                }
+                else
+                {
+                     isOtherJump = false;
+                     jumpTime = 0.0f;
+                }
+           }
+           //地面にいるとき
+           else if (isGround)
+           {
+                if (verticalKey > 0)
+                {
+                    ySpeed = jumpSpeed;
+                    jumpPos = transform.position.y; //ジャンプした位置を記録する
+                    isJump = true;
+                    jumpTime = 0.0f;
+                }
+                else
+                {
+                    isJump = false;
+                }
+           }
+           //ジャンプ中
+           else if (isJump)
+           {
+                 //上方向キーを押しているか
+　　　　　　    　bool pushUpKey = verticalKey > 0;
+　　　　　　    　//現在の高さが飛べる高さより下か
+　　　　　　    　bool canHeight = jumpPos + jumpHeight > transform.position.y;
+　　　　　　    　//ジャンプ時間が長くなりすぎてないか
+　　　　　　    　bool canTime = jumpLimitTime > jumpTime;
+
+　　　　　　　    if (pushUpKey && canHeight && canTime && !isHead)
+                 {
+                     ySpeed = jumpSpeed;
+                     jumpTime += Time.deltaTime;
+                 }
+                 else
+                 {
+                     isJump = false;
+                     jumpTime = 0.0f;
+                 }
+          }
+
+          if (isJump || isOtherJump)
+          {
+              ySpeed *= jumpCurve.Evaluate(jumpTime);
+          }
+          return ySpeed;
+     }
+
+     /// <summary>
+     /// X成分で必要な計算をし、速度を返す。
+     /// </summary>
+     /// <returns>X軸の速さ</returns>
+     private float GetXSpeed()
+     {
           float horizontalKey = Input.GetAxis("Horizontal");
           float xSpeed = 0.0f;
-          float ySpeed = -gravity;
-          float verticalKey = Input.GetAxis("Jump");
-
-          if (isGround)
-          {
-              if (verticalKey > 0)
-              {
-                  ySpeed = jumpSpeed;
-                  jumpPos = transform.position.y; //ジャンプした位置を記録する
-                  isJump = true;
-                  jumpTime = 0.0f;
-              }
-              else
-              {
-                  isJump = false;
-              }
-          }
-          else if (isJump)
-          {
-              //上方向キーを押しているか
-　　　　　　　bool pushUpKey = verticalKey > 0;
-　　　　　　　//現在の高さが飛べる高さより下か
-　　　　　　　bool canHeight = jumpPos + jumpHeight > transform.position.y;
-　　　　　　　//ジャンプ時間が長くなりすぎてないか
-　　　　　　　bool canTime = jumpLimitTime > jumpTime;
-
-　　　　　　　if (pushUpKey && canHeight && canTime && !isHead)
-              {
-                  ySpeed = jumpSpeed;
-                  jumpTime += Time.deltaTime;
-              }
-              else
-              {
-                  isJump = false;
-                  jumpTime = 0.0f;
-              }
-          }
-
           if (horizontalKey > 0)
           {
               transform.localScale = new Vector3(5, 5, 5);
-              anim.SetBool("run", true);
-              dashTime += Time.deltaTime;  //New
+              isRun = true;
+              dashTime += Time.deltaTime;
               xSpeed = speed;
           }
           else if (horizontalKey < 0)
           {
               transform.localScale = new Vector3(-5, 5, 5);
-              anim.SetBool("run", true);
-              dashTime += Time.deltaTime;  //New
+              isRun = true;
+              dashTime += Time.deltaTime;
               xSpeed = -speed;
           }
           else
           {
-              anim.SetBool("run", false);
+              isRun = false;
               xSpeed = 0.0f;
-              dashTime = 0.0f;  //New
+              dashTime = 0.0f;
           }
 
-          //前回の入力からダッシュの反転を判断して速度を変える New
+          //前回の入力からダッシュの反転を判断して速度を変える
           if (horizontalKey > 0 && beforeKey < 0)
           {
               dashTime = 0.0f;
@@ -108,16 +218,101 @@ using System.Collections;
           {
               dashTime = 0.0f;
           }
-          beforeKey = horizontalKey;
 
-          //アニメーションカーブを速度に適用 New
+          beforeKey = horizontalKey;
           xSpeed *= dashCurve.Evaluate(dashTime);
-          if (isJump)
+          beforeKey = horizontalKey;
+          return xSpeed;
+     }
+
+     /// <summary>
+     /// アニメーションを設定する
+     /// </summary>
+     private void SetAnimation()
+     {
+          anim.SetBool("jump", isJump || isOtherJump);
+          anim.SetBool("ground", isGround);
+          anim.SetBool("run", isRun);
+     }
+
+    /// <summary>
+　　/// コンティニュー待機状態か
+    /// </summary>
+    /// <returns></returns>
+    public bool IsContinueWaiting()
+    {
+        return IsDownAnimEnd();
+    }
+
+    //ダウンアニメーションが完了しているかどうか
+    private bool IsDownAnimEnd()
+    {
+        if(isDown && anim != null)
+        {
+             AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+             if (currentState.IsName("damage"))
+             {
+                  if(currentState.normalizedTime >= 1)
+                  {
+                       return true;
+                  }
+             }
+         }
+         return false;
+    }
+
+     /// <summary>
+     /// コンティニューする
+     /// </summary>
+     public void ContinuePlayer()
+     {
+          isDown = false;
+          anim.Play("stand");
+          isJump = false;
+          isOtherJump = false;
+          isRun = false;
+          isContinue = true;
+     }
+
+     #region//接触判定
+     private void OnCollisionEnter2D(Collision2D collision)
+     {
+          if (collision.collider.tag == enemyTag)
           {
-              ySpeed *= jumpCurve.Evaluate(jumpTime);
+              //踏みつけ判定になる高さ
+              float stepOnHeight = (capcol.size.y * (stepOnRate / 100f));
+
+              //踏みつけ判定のワールド座標
+              float judgePos = transform.position.y - (capcol.size.y / 2f) + stepOnHeight;
+
+              foreach (ContactPoint2D p in collision.contacts)
+              {
+                  if (p.point.y < judgePos)
+                  {
+                      ObjectCollision o = collision.gameObject.GetComponent<ObjectCollision>();
+
+                       if (o != null)
+                      {
+                          otherJumpHeight = o.boundHeight;    //踏んづけたものから跳ねる高さを取得する
+                          o.playerStepOn = true;        //踏んづけたものに対して踏んづけた事を通知する
+                          jumpPos = transform.position.y; //ジャンプした位置を記録する
+                          isOtherJump = true;
+                          isJump = false;
+                          jumpTime = 0.0f;
+                      }
+                      else
+                      {
+                          Debug.Log("ObjectCollisionが付いてないよ!");
+                      }
+                  }
+                  else
+                  {
+                      anim.Play("damage");
+                      isDown = true;
+                      break;
+                  }
+              }
           }
-          anim.SetBool("jump", isJump); //New
-          anim.SetBool("ground", isGround); //New
-          rb.velocity = new Vector2(xSpeed, ySpeed);
-      }
+     }
+     #endregion
  }
