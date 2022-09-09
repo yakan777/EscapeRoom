b25a282817bc05f4c5bb9c222e2a71beb0845321
noriokaka <aki.na@live.jp>
2022-09-09 15:22:15 +0900

    統合 完了

--- Assets/Nagatsu/script/ZombieGimick.cs
+++ Assets/Nagatsu/script/ZombieGimick.cs
@@ -6,6 +6,8 @@ public class ZombieGimick : MonoBehaviour
 {
     public GameObject passwordnumber;
     public NagatsuItem.Type clearItem;
+    public AudioClip sound4;
+    AudioSource audioSource;
     private void Start() {
         passwordnumber.SetActive(false);
     }
@@ -15,6 +17,8 @@ public class ZombieGimick : MonoBehaviour
         {
             GetComponent<BoxCollider>().enabled = false;
             passwordnumber.SetActive(true);
+            audioSource = GetComponent<AudioSource>();
+            audioSource.PlayOneShot(sound4);
 
         }
 
