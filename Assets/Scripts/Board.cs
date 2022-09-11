6ce4a3e13f87b7ac99e7a08b84d40bb61414c5e8
sasakiy916 <sasaki3y98521j@gmail.com>
2022-09-12 01:23:46 +0900

    Board.cs 修正

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class Board : MonoBehaviour, IPointerClickHandler
 {
-    bool canOpen;//開けられるかどうか
+    public bool canOpen;//開けられるかどうか
     bool isOpen;//開いてる状態か
     public bool isOpenIn;//内開きかなのか
     float rotY;//開き具合
